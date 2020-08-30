7e1c34a3828f68fbe69decc410d3eec6da31ff6c
Jask Tin <jasktintran@gmail.com>
2020-08-31 01:35:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '123cuoicai',
+      database : 'smart-brain'
+    }
+});
+
+app.get ('/', (req, res) => res.send(database.users));
+app.post('/signin', (req, res) => { signin.handleRegister(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get ('/profile:id', (req, res) => { register.handleGetProfile(req, res, db) });
+app.put ('/image', (req, res) => { image.handleImage(req, res, db) } );
+
+/*
+
+</>             res = this is working
+</signin>       POST = success / fail
+</register>     POST = user
+</profile/:id>  GET = user:id
+</image>        PUT = user:updated 
+
+*/
