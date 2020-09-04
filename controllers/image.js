220fa81f1bc4c8e94514e27b095ac3068192dd39
Jask Tin <jasktintran@gmail.com>
2020-09-05 01:10:48 +0700

    .

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+    apiKey: '30b99d22164847759d4b066fa4769302'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+  }
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id).increment('entries', 1)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
