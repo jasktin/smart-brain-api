8af1e5f84384c9af7eb60ec3ce6ace972741f758
Jask Tin <jasktintran@gmail.com>
2020-09-05 01:43:58 +0700

    .

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,16 @@
-const handleGetProfile = (req, res, db) => {
+const handleProfileGet = (req, res, db) => {
     const { id } = req.params;
     db.select('*').from('users').where({id})
-    .then(user => {
+      .then(user => {
         if (user.length) {
-            res.json(user[0]);
+          res.json(user[0])
         } else {
-            res.status(400).json('Not Found')
+          res.status(400).json('Not found')
         }
-    })
-    .catch(err => res.status(400).json('Error Getting User')) 
-}
-
-module.exports = {
-    handleGetProfile: handleGetProfile
-}
+      })
+      .catch(err => res.status(400).json('error getting user'))
+  }
+  
+  module.exports = {
+    handleProfileGet
+  }
