c61031cf8e9aaf442b3d10aa0811f9fdfd319da7
Jask Tin <jasktintran@gmail.com>
2020-09-05 12:34:06 +0700

    .

--- server.js
+++ server.js
@@ -5,34 +5,31 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-const auth = require('./controllers/authorization');
-
-// Database Set-up
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
-});
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) });
-app.post('/signin', signin.signinAuthentication(db, bcrypt));
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)});
-app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)});
-app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)});
-app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)});
+const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+});
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt)});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT, () => {
   console.log(`App is running on port ${process.env.PORT}`)
