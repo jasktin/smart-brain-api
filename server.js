19c055a7f97c00a4dca168f70aa867016d081d9e
Jask Tin <jasktintran@gmail.com>
2020-09-05 11:55:56 +0700

    .

--- server.js
+++ server.js
@@ -5,27 +5,28 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+// Database Set-up
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+});
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-});
-
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt)});
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
