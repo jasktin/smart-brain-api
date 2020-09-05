1f05f77aba761d69b62227197f3695d4dd53a1d0
Jask Tin <jasktintran@gmail.com>
2020-09-05 13:02:56 +0700

    .

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const db = knex({
     }
 });
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('It is working') })
 app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt)});
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
